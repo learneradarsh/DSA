6f907b1dac06c6e7fed9de04d8f3f653e7e3123c
learneradarsh <learner.adarsh@gmail.com>
2020-01-17 14:25:40 +0530

    INITIALISED: util.js

---
+++ util.js
@@ -0,0 +1,21 @@
+class Util{
+
+    //static method to find two input given strings are anagram or not
+    static isAnagram(stringOne,stringTwo) {
+        
+        //If both strings length is not same then they can't be anagram 
+        if(stringOne.length != stringTwo.length){
+            return false;
+        }
+       
+        //If both sorted strings are not equal then they can't be anagram 
+        if(stringOne.split('').sort().join('') != stringTwo.split('').sort().join('')){
+            return false;
+        }
+        return true;
+    }
+}
+
+//console.log(Util.isAnagram("abc","abc"));
+
+  
