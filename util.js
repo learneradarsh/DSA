4d669d59b9136cc93b9ad3bba20283358226ed5b
learneradarsh <learner.adarsh@gmail.com>
2020-01-17 14:57:03 +0530

    MODIFIED: ADDED isPrime method
    Implemented isPrime static method to check whether input given number is prime or not.

--- util.js
+++ util.js
@@ -1,21 +1,40 @@
-class Util{
+class Util {
 
     //static method to find two input given strings are anagram or not
-    static isAnagram(stringOne,stringTwo) {
-        
+    static isAnagram(stringOne, stringTwo) {
+
         //If both strings length is not same then they can't be anagram 
-        if(stringOne.length != stringTwo.length){
+        if (stringOne.length != stringTwo.length) {
             return false;
         }
-       
+
         //If both sorted strings are not equal then they can't be anagram 
-        if(stringOne.split('').sort().join('') != stringTwo.split('').sort().join('')){
+        if (stringOne.split('').sort().join('') != stringTwo.split('').sort().join('')) {
             return false;
         }
 
         return true;
     }
+
+    //static method to find prime number between 0-1000
+    static isPrime(number) {
+
+        if (!isNaN(number)) {
+            if (number <= 1) {
+                return false;
+            }
+            for (let i = 2; i < number; i++) {
+                if (number % i == 0) {
+                    return false;
+                }
+            }
+            return true;
+        } else {
+            console.log("You have entered wrong input");
+            return false;
+        }
+
+    }
 }
 
-
-  
+console.log(Util.isPrime(7));
