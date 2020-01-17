f808bdeabe4da9a39d46bf9ff3d45a7be87338e5
learneradarsh <learner.adarsh@gmail.com>
2020-01-17 15:40:19 +0530

    MODIFIED: ADDED isAnagramAndPalindrome method
    Implemented isAnagramAndPalindrome(numberOne,numberTwo) to check whether numbers are prime, Anagram, and Palindrome or not

--- util.js
+++ util.js
@@ -2,7 +2,8 @@ class Util {
 
     //static method to find two input given strings are anagram or not
     static isAnagram(stringOne, stringTwo) {
-
+        stringOne = stringOne.toString();
+        stringTwo = stringTwo.toString();
         //If both strings length is not same then they can't be anagram 
         if (stringOne.length != stringTwo.length) {
             return false;
@@ -42,6 +43,18 @@ class Util {
         numberTwo = numberTwo.toString();
         if (numberOne.split('').reverse().join('') == numberTwo) {
             return true;
+        }
+        return false;
+    }
+
+    //static method to find prime numbers that are anagram and palindrome
+    static isAnagramAndPalindrome(numberOne, numberTwo) {
+        if (this.isPrime(numberOne) && this.isPrime(numberTwo)) {
+            if (this.isAnagram(numberOne, numberTwo)) {
+                if (this.isPalindrome(numberOne, numberTwo)) {
+                    return true;
+                }
+            }
         }
         return false;
     }
