df1b4a8f30d584d1ebae451ca422ac2aa879c017
learneradarsh <learner.adarsh@gmail.com>
2020-01-17 13:46:02 +0530

    REMOVED: 2dArray.js

--- 2dArray.js
+++
@@ -1,9 +0,0 @@
-let standard_input=process.stdin;
-console.log("Enter row:");
-standard_input.on("data",function(data){
-    if(data === "exit\n"){
-        process.exit();
-    } else{
-
-    }
-});
