74d382e3348d533b0e08157c8cf78f8121154bb9
Adarsh Learner <learner.adarsh@gmail.com>
2020-01-17 10:40:04 +0530

    INITIALISED: 2dArray.js

---
+++ 2dArray.js
@@ -0,0 +1,9 @@
+let standard_input=process.stdin;
+console.log("Enter row:");
+standard_input.on("data",function(data){
+    if(data === "exit\n"){
+        process.exit();
+    } else{
+
+    }
+});
