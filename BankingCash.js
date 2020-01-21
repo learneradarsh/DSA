9628434fff4f6d560b4a75281105e2d0ba17abf0
learneradarsh <learner.adarsh@gmail.com>
2020-01-21 14:50:58 +0530

    MODIFIED: ADDED deposit and withdraw logic
    Implemented deposit and withdraw queue to show the one day banking transaction.

--- BankingCash.js
+++ BankingCash.js
@@ -1,36 +1,40 @@
+let standard_input = process.stdin;
+
 class Queue {
     constructor(maxSize) { // maxSize denotes size of queueu
         this.front = 0;
         this.rear = 0;
-        this.maxSize = 0;
-        this.queue = new Array(maxSize);
+        this.maxSize = maxSize;
+        // this.queue = new Array(maxSize);
+        this.queue = [];
     }
 
     //method to add item to the rear of the queue
     enqueue(item) {
-        if(this.maxSize == this.rear){
+        if (this.maxSize == this.rear && this.rear != 0) {
             return -1;
-        }else{
-            this.queue[this.rear]=item;
+        } else {
+            this.queue[this.rear] = item;
+            this.rear++;
         }
-     }
+    }
 
     //method to remove item from the front of the queue
     dequeue() {
         //isEmpty
-        if(this.front == this.rear){
+        if (this.front == this.rear) {
             return -1;
-        }else{
-            for(let i=0;i<this.rear-1;i++){
-                this.queue[i]=this.queue+1;
+        } else {
+            for (let i = 0; i < this.rear - 1; i++) {
+                this.queue[i] = this.queue[i + 1];
             }
 
-            if(this.rear<this.maxSize){
-                this.queue[this.rear]=0;
+            if (this.rear < this.maxSize) {
+                this.queue[this.rear] = 0;
             }
-            this.rear--
+            this.rear--;
         }
-     }
+    }
 
     //method to check queue is empty or not
     isEmpty() {
@@ -46,7 +50,54 @@ class Queue {
     }
 
     //method to print queue
-    printQueue(){
-        return this.queue;
+    printQueue() {
+        let finalOutput=" ";
+        for(let x of this.queue){
+            finalOutput=finalOutput + x + "\t";
+        }
+        return finalOutput;
     }
 }
+
+const deposit = new Queue(20);
+const withdraw = new Queue(20);
+
+
+function main() {
+    console.log(`
+    Welcome to Banking System
+    -------------------------
+    Select Your Option
+    1. Deposit
+    2. WithDraw
+    3. Exit
+`);}
+
+    standard_input.on("data", function (data) {
+        if (data.toString().trim() === "exit") {
+            process.exit();
+        } else {
+            switch (data.toString().trim()) {
+                case "1":
+                    console.log("Your Transact Id is :", new Date());
+                    deposit.enqueue(parseInt(Math.random() * 100000));
+                    main();
+                    break;
+                case "2":
+                    console.log("Your Transact Id is :", new Date());
+                    withdraw.enqueue(parseInt(Math.random() * 1000));
+                    main();
+                    break;
+                case "3":
+                    console.log("Total Amount Deposited:",deposit.printQueue());
+                    console.log("Total Amount Withdrawn:",withdraw.printQueue());
+                    process.exit();
+                default:
+                    console.log("Something went wrong");
+                    process.exit();
+            }
+        }
+    });
+
+
+main();
