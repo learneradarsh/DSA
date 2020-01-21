ed7717d9887489b1eb9798d75c03555d08256052
learneradarsh <learner.adarsh@gmail.com>
2020-01-21 17:54:10 +0530

    MODIFIED: ADDED merge(left,right) and mergeSort(inpArr)
    Implemented merge and mergeSort method static method

--- mergeSort.js
+++ mergeSort.js
@@ -1,8 +1,33 @@
-class MergeSort{
-    static merge(){
-        
-    }
-    static mergeSort(){
+let standard_input = process.stdin;
+let inpArr=[];
+class MergeSort {
 
+    //method to merge arrays
+    static merge(left, right) {
+        let resArr = [], leftIn = 0, rightIn = 0;
+
+        while (leftIn < left.length && rightIn < right.length) {
+            if (left[leftIn] < right[rightIn]) {
+                resArr.push(left[leftIn]);
+                leftIn++;
+            } else {
+                resArr.push(right[rightIn]);
+                rightIn++;
+            }
+        }
+
+        return resArr.concat(left.slice(leftIn)).concat(right.slice(rightIn));
+    }
+
+    //method to perform merge sort on inpArr
+    static mergeSort(inpArr) {
+        if (inpArr.length <= 1) {
+            return inpArr;
+        }
+        const mid = Math.floor(inpArr.length / 2);
+        const left = inpArr.slice(0, mid);
+        const right = inpArr.slice(mid);
+
+        return this.merge(this.mergeSort(left), this.mergeSort(right));
     }
 }
