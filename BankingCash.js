b03c236bed19e08df8e58ee7df95054eb47f7982
learneradarsh <learner.adarsh@gmail.com>
2020-01-21 12:06:21 +0530

    INITIALISED: BankingCash.js

---
+++ BankingCash.js
@@ -0,0 +1,19 @@
+class Queue{
+    constructor(){
+
+    }
+
+    //method to add item to the rear of the queue
+    enqueue(item){}
+
+    //method to remove item from the front of the queue
+    dequeue(){}
+
+    //method to check queue is empty or not
+    isEmpty(){}
+
+    //method to check size of queue
+    size(){
+        
+    }
+}
