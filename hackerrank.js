c0aef54fe5598e41fa7277b66c914c71fe52a8ae
learneradarsh <learner.adarsh@gmail.com>
2020-01-21 11:16:22 +0530

    MODIFIED: ADDED INPUT VALIDATION
    Implemented logic to check input array containing any NaN or not

--- hackerrank.js
+++ hackerrank.js
@@ -5,17 +5,17 @@ class BSTCount {
     constructor() {
         this.n = 0;
     }
-    fact(n){
-        let f=1;
-        for (let i = 1 ; i <= n ; i++ ){
-            f=f*i;
+    fact(n) {
+        let f = 1;
+        for (let i = 1; i <= n; i++) {
+            f = f * i;
         }
         return f;
     }
     countBST(n) {
 
         //finding Cn using Catalan number formula
-        let Cn = this.fact(2*n) / (this.fact(n+1) * this.fact(n));
+        let Cn = this.fact(2 * n) / (this.fact(n + 1) * this.fact(n));
         return parseInt(Cn);
     }
 }
@@ -26,8 +26,12 @@ standard_input.on("data", function (data) {
     } else {
         const bst = new BSTCount();
         inputArr = data.toString().trim().split(' ');
-        for(let i=0;i<inputArr.length;i++){
-            console.log(`No. of BSTs can form for ${inputArr[i]} Node =`,bst.countBST(parseInt(inputArr[i])));
+        for (let i = 0; i < inputArr.length; i++) {
+            if(isNaN(inputArr[i])){
+                console.log(`${inputArr[i]} is invalid element, it should be number`);
+                process.exit();
+            }
+            console.log(`No. of BSTs can form for ${inputArr[i]} Node =`, bst.countBST(parseInt(inputArr[i])));
         }
         process.exit();
     }
