0fca50a2cdcd4227ee493b8d7ec93964f1af9d49
learneradarsh <learner.adarsh@gmail.com>
2020-01-21 12:26:20 +0530

    MODIFIED: ADDED Queue Class
    Implemented enqueue, dequeue, isEmpty, size, printQueue method for queue operations

--- BankingCash.js
+++ BankingCash.js
@@ -1,19 +1,52 @@
-class Queue{
-    constructor(){
-
+class Queue {
+    constructor(maxSize) { // maxSize denotes size of queueu
+        this.front = 0;
+        this.rear = 0;
+        this.maxSize = 0;
+        this.queue = new Array(maxSize);
     }
 
     //method to add item to the rear of the queue
-    enqueue(item){}
+    enqueue(item) {
+        if(this.maxSize == this.rear){
+            return -1;
+        }else{
+            this.queue[this.rear]=item;
+        }
+     }
 
     //method to remove item from the front of the queue
-    dequeue(){}
+    dequeue() {
+        //isEmpty
+        if(this.front == this.rear){
+            return -1;
+        }else{
+            for(let i=0;i<this.rear-1;i++){
+                this.queue[i]=this.queue+1;
+            }
+
+            if(this.rear<this.maxSize){
+                this.queue[this.rear]=0;
+            }
+            this.rear--
+        }
+     }
 
     //method to check queue is empty or not
-    isEmpty(){}
+    isEmpty() {
+        if (this.front == this.rear) {
+            return true;
+        }
+        return false;
+    }
 
     //method to check size of queue
-    size(){
-        
+    size() {
+        return this.queue.length;
+    }
+
+    //method to print queue
+    printQueue(){
+        return this.queue;
     }
 }
